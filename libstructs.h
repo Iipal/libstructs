9191829fd4caef4d13c1ed4a18ac7f586fdff50a
Iipal <malkoleyplay@gmail.com>
2019-12-11 21:25:51 +0200

    update blya

--- libstructs.h
+++ libstructs.h
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/15 16:45:57 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/11/18 01:19:52 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/12/11 21:25:22 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,16 +25,13 @@
 #  define LIBS_NO_VOIDPTR_STRUCTS
 # endif /* LIBS_NO_STRUCTS */
 
-# include <sys/cdefs.h>
-# ifndef libs_always_inline
-#  if defined __header_always_inline
-#   define libs_always_inline __header_always_inline
-#  elif defined __always_inline
-#   define libs_always_inline __always_inline
-#  else
-#   define libs_always_inline
-#  endif
-# endif
+# ifndef libs_static_inline
+#  define libs_static_inline static inline
+# endif /* libs_static_inline */
+
+#ifndef libs_attributes
+#  define libs_attributes __attribute__((__const__,__pure__,__always_inline__))
+#endif /* libs_attributes */
 
 # ifndef LIBS_CREATE_STRUCT2
 #  define LIBS_CREATE_STRUCT2(name, type) \
@@ -59,128 +56,131 @@
 
 # ifndef LIBS_INIT_STRUCT2
 #  define LIBS_INIT_STRUCT2(name, type) \
-	s2##name libs_always_inline \
-	libs_init_s2##name##__(type x__, type y__) { \
-		return (s2##name) { x__, y__ }; }
+	libs_static_inline s2##name libs_attributes \
+	s2##name##_init_(const type x_, const type y_) \
+		{ return (s2##name){ x_, y_ }; }
 # endif /* LIBS_INIT_STRUCT2 */
 
 # ifndef LIBS_INIT_STRUCT3
 #  define LIBS_INIT_STRUCT3(name, type) \
-	s3##name libs_always_inline \
-	libs_init_s3##name##__(type x__, type y__, type z__) { \
-		return (s3##name) { x__, y__, z__ }; }
+	libs_static_inline s3##name libs_attributes \
+	s3##name##_init_(const type x_, const type y_, \
+							const type z_) \
+		{ return (s3##name){ x_, y_, z_ }; }
 # endif /* LIBS_INIT_STRUCT3 */
 
 # ifndef LIBS_INIT_STRUCT4
 #  define LIBS_INIT_STRUCT4(name, type) \
-	s4##name libs_always_inline \
-	libs_init_s4##name##__(type x__, type y__, type z__, type w__) { \
-		return (s4##name) { x__, y__, z__, w__ }; }
+	libs_static_inline s4##name libs_attributes \
+	s4##name##_init_(const type x_, const type y_, \
+					const type z_, const type w_) \
+		{ return (s4##name){ x_, y_, z_, w_ }; }
 # endif /* LIBS_INIT_STRUCT4 */
 
 # ifndef LIBS_STRUCT2_TO_STRUCT3
 #  define LIBS_STRUCT2_TO_STRUCT3(name, type) \
-	s3##name libs_always_inline \
-	libs_s2##name##_to_s3##name##__(s2##name src__, type z__) { \
-		return (s3##name) { src__.x, src__.y, z__ }; }
+	libs_static_inline s3##name libs_attributes \
+	s2##name##_to_s3##name##_(const s2##name src_, const type z_) \
+		{ return (s3##name){ src_.x, src_.y, z_ }; }
 # endif /* LIBS_STRUCT2_TO_STRUCT3 */
 
 # ifndef LIBS_STRUCT2_TO_STRUCT4
 #  define LIBS_STRUCT2_TO_STRUCT4(name, type) \
-	s4##name libs_always_inline \
-	libs_s2##name##_to_s4##name##__(s2##name src__, type z__, type w__) { \
-		return (s4##name) { src__.x, src__.y, z__, w__}; }
+	libs_static_inline s4##name libs_attributes \
+	s2##name##_to_s4##name##_(const s2##name src_, \
+					const type z_, const type w_) \
+		{ return (s4##name){ src_.x, src_.y, z_, w_}; }
 # endif /* LIBS_STRUCT2_TO_STRUCT4 */
 
 # ifndef LIBS_STRUCT3_TO_STRUCT2
 #  define LIBS_STRUCT3_TO_STRUCT2(name) \
-	s2##name libs_always_inline \
-		libs_s3##name##_to_s2##name##__(s3##name src__) { \
-			return (s2##name) { src__.x, src__.y, }; }
+	libs_static_inline s2##name libs_attributes \
+	s3##name##_to_s2##name##_(const s3##name src_) \
+		{ return (s2##name){ src_.x, src_.y, }; }
 # endif /* LIBS_STRUCT3_TO_STRUCT2 */
 
 # ifndef LIBS_STRUCT3_TO_STRUCT4
 #  define LIBS_STRUCT3_TO_STRUCT4(name, type) \
-	s4##name libs_always_inline \
-	libs_s3##name##_to_s4##name##__(s3##name src__, type w__) { \
-		return (s4##name) { src__.x, src__.y, src__.z, w__ }; }
+	libs_static_inline s4##name libs_attributes \
+	s3##name##_to_s4##name##_(const s3##name src_, const type w_) \
+		{ return (s4##name){ src_.x, src_.y, src_.z, w_ }; }
 # endif /* LIBS_STRUCT3_TO_STRUCT4 */
 
 # ifndef LIBS_STRUCT4_TO_STRUCT3
 #  define LIBS_STRUCT4_TO_STRUCT3(name) \
-	s3##name libs_always_inline \
-	libs_s4##name##_to_s3##name##__(s4##name src__) { \
-		return (s3##name) { src__.x, src__.y, src__.z }; }
+	libs_static_inline s3##name libs_attributes \
+	s4##name##_to_s3##name##_(const s4##name src_) \
+		{ return (s3##name){ src_.x, src_.y, src_.z }; }
 # endif /* LIBS_STRUCT4_TO_STRUCT3 */
 
 # ifndef LIBS_STRUCT4_TO_STRUCT2
 #  define LIBS_STRUCT4_TO_STRUCT2(name) \
-	s2##name libs_always_inline \
-	libs_s4##name##_to_s2##name##__(s4##name src__) { \
-		return (s2##name) { src__.x, src__.y }; }
+	libs_static_inline s2##name libs_attributes \
+	s4##name##_to_s2##name##_(const s4##name src_) \
+		{ return (s2##name){ src_.x, src_.y }; }
 # endif /* LIBS_STRUCT4_TO_STRUCT2 */
 
 # ifndef LIBS_STRUCT2_ADD_STRUCT2
 #  define LIBS_STRUCT2_ADD_STRUCT2(name) \
-	s2##name libs_always_inline \
-	libs_s2##name##_add_s2##name##__(s2##name a__, s2##name b__) { \
-		return (s2##name) { a__.x + b__.x, a__.y + b__.y }; }
+	libs_static_inline s2##name libs_attributes \
+	s2##name##_add_s2##name##_(const s2##name a__, const s2##name b__) \
+		{ return (s2##name){ a__.x + b__.x, a__.y + b__.y }; }
 # endif /* LIBS_STRUCT2_ADD_STRUCT2 */
 
 # ifndef LIBS_STRUCT2_ADD_STRUCT3
 #  define LIBS_STRUCT2_ADD_STRUCT3(name) \
-	s2##name libs_always_inline \
-	libs_s2##name##_add_s3##name##__(s2##name a__, s3##name b__) { \
-		return (s2##name) { a__.x + b__.x, a__.y + b__.y }; }
+	libs_static_inline s2##name libs_attributes \
+	s2##name##_add_s3##name##_(const s2##name a__, const s3##name b__) \
+		{ return (s2##name){ a__.x + b__.x, a__.y + b__.y }; }
 # endif /* LIBS_STRUCT2_ADD_STRUCT3 */
 
 # ifndef LIBS_STRUCT2_ADD_STRUCT4
 #  define LIBS_STRUCT2_ADD_STRUCT4(name) \
-	s2##name libs_always_inline \
-	libs_s2##name##_add_s4##name##__(s2##name a__, s4##name b__) { \
-		return (s2##name) { a__.x + b__.x, a__.y + b__.y }; }
+	libs_static_inline s2##name libs_attributes \
+	s2##name##_add_s4##name##_(const s2##name a__, const s4##name b__) \
+		{ return (s2##name){ a__.x + b__.x, a__.y + b__.y }; }
 # endif /* LIBS_STRUCT2_ADD_STRUCT4 */
 
 # ifndef LIBS_STRUCT3_ADD_STRUCT2
 #  define LIBS_STRUCT3_ADD_STRUCT2(name) \
-	s3##name libs_always_inline \
-	libs_s3##name##_add_s2##name##__(s3##name a__, s2##name b__) { \
-		return (s3##name) { a__.x + b__.x, a__.y + b__.y, 0 }; }
+	libs_static_inline s3##name libs_attributes \
+	s3##name##_add_s2##name##_(const s3##name a__, const s2##name b__) \
+		{ return (s3##name){ a__.x + b__.x, a__.y + b__.y, 0 }; }
 # endif /* LIBS_STRUCT3_ADD_STRUCT2 */
 
 # ifndef LIBS_STRUCT3_ADD_STRUCT3
 #  define LIBS_STRUCT3_ADD_STRUCT3(name) \
-	s3##name libs_always_inline \
-	libs_s3##name##_add_s3##name##__(s3##name a__, s3##name b__) { \
-		return (s3##name) { a__.x + b__.x, a__.y + b__.y, a__.z + b__.z }; }
+	libs_static_inline s3##name libs_attributes \
+	s3##name##_add_s3##name##_(const s3##name a__, const s3##name b__) \
+		{ return (s3##name){ a__.x + b__.x, a__.y + b__.y, a__.z + b__.z }; }
 # endif /* LIBS_STRUCT3_ADD_STRUCT3 */
 
 # ifndef LIBS_STRUCT3_ADD_STRUCT4
 #  define LIBS_STRUCT3_ADD_STRUCT4(name) \
-	s3##name libs_always_inline \
-	libs_s3##name##_add_s4##name##__(s3##name a__, s4##name b__) { \
-		return (s3##name) { a__.x + b__.x, a__.y + b__.y, a__.z + b__.z }; }
+	libs_static_inline s3##name libs_attributes \
+	s3##name##_add_s4##name##_(const s3##name a__, const s4##name b__) \
+		{ return (s3##name){ a__.x + b__.x, a__.y + b__.y, a__.z + b__.z }; }
 # endif /* LIBS_STRUCT3_ADD_STRUCT4 */
 
 # ifndef LIBS_STRUCT4_ADD_STRUCT2
 #  define LIBS_STRUCT4_ADD_STRUCT2(name) \
-	s4##name libs_always_inline \
-	libs_s4##name##_add_s2##name##__(s4##name a__, s2##name b__) { \
-		return (s4##name) { a__.x + b__.x, a__.y + b__.y, 0, 0 }; }
+	libs_static_inline s4##name libs_attributes \
+	s4##name##_add_s2##name##_(const s4##name a__, const s2##name b__) \
+		{ return (s4##name){ a__.x + b__.x, a__.y + b__.y, 0, 0 }; }
 # endif /* LIBS_STRUCT4_ADD_STRUCT2 */
 
 # ifndef LIBS_STRUCT4_ADD_STRUCT3
 #  define LIBS_STRUCT4_ADD_STRUCT3(name) \
-	s4##name libs_always_inline \
-	libs_s4##name##_add_s3##name##__(s4##name a__, s3##name b__) { \
-		return (s4##name) { a__.x + b__.x, a__.y + b__.y, a__.z + b__.z, 0 }; }
+	libs_static_inline s4##name libs_attributes \
+	s4##name##_add_s3##name##_(const s4##name a__, const s3##name b__) \
+		{ return (s4##name){ a__.x + b__.x, a__.y + b__.y, a__.z + b__.z, 0 }; }
 # endif /* LIBS_STRUCT4_ADD_STRUCT3 */
 
 # ifndef LIBS_STRUCT4_ADD_STRUCT4
 #  define LIBS_STRUCT4_ADD_STRUCT4(name) \
-	s4##name libs_always_inline \
-		libs_s4##name##_add_s4##name##__(s4##name a__, s4##name b__) { \
-			return (s4##name) { a__.x + b__.x, a__.y + b__.y, \
+	libs_static_inline s4##name libs_attributes \
+		s4##name##_add_s4##name##_(const s4##name a__, const s4##name b__) \
+			{ return (s4##name){a__.x + b__.x, a__.y + b__.y, \
 								a__.z + b__.z, a__.w + b__.w, }; }
 # endif /* LIBS_STRUCT4_ADD_STRUCT4 */
 
@@ -328,7 +328,10 @@ LIBS_CREATE_234_STRUCT(ld, double) /* long double: s2ld;s3ld;s4ld */
 #  undef LIBS_STRUCT4_ADD_STRUCT4
 # endif /* LIBS_STRUCT4_ADD_STRUCT4 */
 
-# ifdef libs_always_inline
-#  undef libs_always_inline
-# endif /* libs_always_inline */
+# ifdef libs_static_inline
+#  undef libs_static_inline
+# endif /* libs_static_inline */
+# ifdef libs_attributes
+#  undef libs_attributes
+# endif /* libs_attributes */
 #endif /* LIBSTRUCTS_H */
